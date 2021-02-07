630825fa6c412828cfea9e99c17ec1e4955bb156
Nhan Dang <nhantdang@gmail.com>
2021-02-07 15:01:34 +1100

    WIP

---
+++ kata_word_chains/IDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace kata_word_chains
+{
+    public interface IDictionary
+    {
+
+        bool WordIsValid(string word);
+
+        string GetValidWord(string startingWord, string endingWord, int indexOfLetterToReplace);
+
+    }
+}
---
+++ kata_word_chains_tests/MockDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using kata_word_chains;
+
+namespace kata_word_chains_tests
+{
+    public class MockDictionary : IDictionary
+    {
+        public MockDictionary()
+        {
+        }
+
+        public string GetValidWord(string startingWord, string endingWord, int indexOfLetterToReplace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WordIsValid(string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
